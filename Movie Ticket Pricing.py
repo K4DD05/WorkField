37c6c1f0cf32c284bd481134e95e214789f4160f
Kingsley Korankye Baidu <102754766+kkbaidu@users.noreply.github.com>
2025-07-14 17:19:40 +0000

    changes to make

--- Movie Ticket Pricing.py
+++ Movie Ticket Pricing.py
@@ -1,5 +1,10 @@
 #Movie Ticket Pricing. We would be calculating the price of a movie ticket based on age of the buyer.
 BuyerAge = int(input("Enter Your Age: "))
+
+# Do not start a variable name with a capital letter.
+# "MovieTicket" should be declared before it is used.
+# Handle the situation where the user inputs an unexpected value for "BuyerAge". eg. -2.
+
 if BuyerAge <13:
     MovieTicket = 5.00
 elif BuyerAge < 18:
